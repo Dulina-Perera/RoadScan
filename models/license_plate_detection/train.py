9787be84173736022c1c1fd7ea739d315ad816f8
Dulina-Perera <dulina.perera@outlook.com>
2024-08-25 19:04:28 +0530

    Designed a script to train a `yolov10` object detector for license plate detection.

--- models/license_plate_detection/train.py
+++ models/license_plate_detection/train.py
@@ -1,13 +1,32 @@
 # %%
-from typing import Any
+import os
+import wandb
+
+from dotenv import load_dotenv
 from ultralytics.models.yolo.model import YOLO
+from wandb.integration.ultralytics import add_wandb_callback
 
 # %%
+# Load environment variables from the .env file.
+load_dotenv()
+
+# Initialize Weights and Biases.
+wandb.login(key=os.getenv('WANDB_API_KEY'), verify=True)
+
+# %%
+# Initialize a Weights and Biases run.
+wandb.init(project='license_plate_detection', job_type='training')
+
 # Load the model.
-model: YOLO = YOLO('yolov8n.yaml', verbose=True)
+model: YOLO = YOLO('yolov10n.yaml', verbose=True)
+
+# Add Weights and Biases callback for Ultralytics.
+add_wandb_callback(model, enable_model_checkpointing=True)
 
 # %%
-results: Any | None = model.train(
+# Train and finetune the model.
+model.train(
+  project='license_plate_detection',
   data='./resources/data.yaml',
   epochs=10,
   batch=64,
@@ -18,3 +37,10 @@ results: Any | None = model.train(
   lr0=0.01,
   patience=3,
 )
+
+# Validate the model.
+model.val()
+
+# %%
+# Finalize the Weights and Biases run.
+wandb.finish()
